e2382529e23bcf0f8281068e9090056825ff4031
Nhamvo <vokimmham123@gmail.com>
2024-08-04 13:19:16 +0700

    comit

---
+++ src/api/content-manager/controllers/content-manager.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * content-manager controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::content-manager.content-manager');
